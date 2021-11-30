dfbc829348b1e541c4a28a0bf9998d569eee0b52
James Taylor <James.Taylor@warp.co.uk>
2021-11-30 11:27:00 +0000

    Fixing a build issue and adding the first test to check CI.

---
+++ src/WARP.XrmSolutionValidator/WARP.XrmSolutionValidator.Core.Tests/Validators/WorkflowScopeTests.cs
@@ -0,0 +1,57 @@
+// <copyright file="UnitTest1.cs" company="WARP Technologies Limited">
+// Released by WARP for use by the CRM development community.
+// </copyright>
+
+namespace WARP.XrmSolutionValidator.Core.Tests.Validators
+{
+    using System;
+    using Xunit;
+    using WARP.XrmSolutionValidator.Core;
+
+    using System.IO;
+    using WARP.XrmSolutionValidator.Core.Models;
+    using WARP.XrmSolutionValidator.Core.Validators;
+    using WARP.XrmSolutionValidator.Core.XrmModels;
+
+    public class WorkflowScopeTests
+    {
+        [Fact]
+        public void SolutionWith1Scope1ReturnsOnly1Warning()
+        {
+            var sit = new WorkflowScope();
+
+            var sln = new XrmSolutionWrapper(null);
+
+            sln.Workflows.Add(new Workflow { Scope = "1" });
+            sln.Workflows.Add(new Workflow { Scope = "2" });
+            sln.Workflows.Add(new Workflow { Scope = "0" });
+            sln.Workflows.Add(new Workflow { Scope = "-1" });
+            sln.Workflows.Add(new Workflow { Scope = "" });
+
+            var result = sit.Validate(sln);
+
+            Assert.True(result.ValidationCompletedSuccessfully);
+            Assert.Equal(1, result.TotalWarnings);
+            Assert.Equal(0, result.TotalErrors);
+        }
+
+        [Fact]
+        public void SolutionWith0Scope1ReturnsNoWarningOrErrors()
+        {
+            var sit = new WorkflowScope();
+
+            var sln = new XrmSolutionWrapper(null);
+
+            sln.Workflows.Add(new Workflow { Scope = "2" });
+            sln.Workflows.Add(new Workflow { Scope = "0" });
+            sln.Workflows.Add(new Workflow { Scope = "-1" });
+            sln.Workflows.Add(new Workflow { Scope = "" });
+
+            var result = sit.Validate(sln);
+
+            Assert.True(result.ValidationCompletedSuccessfully);
+            Assert.Equal(0, result.TotalWarnings);
+            Assert.Equal(0, result.TotalErrors);
+        }
+    }
+}
